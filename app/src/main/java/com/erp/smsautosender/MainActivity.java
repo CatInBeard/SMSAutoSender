5672392fe9fad20bc1c33d85fd8ab975a3dabdfb
Grigoriy Efimov <griefim7987@gmail.com>
2022-12-27 14:54:15 +0300

    Add permission request

--- app/src/main/java/com/erp/smsautosender/MainActivity.java
+++ app/src/main/java/com/erp/smsautosender/MainActivity.java
@@ -1,6 +1,7 @@
 package com.erp.smsautosender;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
@@ -236,8 +237,25 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.SEND_SMS)
                 != PackageManager.PERMISSION_GRANTED) {
-            statusTextView.setText(getResources().getString(R.string.need_sms_permission));
-            primaryButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},100);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 100: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    statusTextView.setText(getResources().getString(R.string.need_sms_permission));
+                    primaryButton.setEnabled(false);
+                }
+
+                return;
+            }
         }
     }
 
